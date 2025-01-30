cb13a301e27e889ac9398fc04d49b68b213c2c51
Hasturious <jbster559@gmail.com>
2025-01-29 18:25:08 -0800

    Add project files.

---
+++ COMP003A.CodingAssignment3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Author: Jean Bryant Figueroa
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Budget management application demonstrating control flow.
+
+namespace COMP003A.CodingAssignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
